c92000245bead31b4e3cb047835253c8fc7be51f
Pamilerin Idowu <pamilerinid@gmail.com>
2019-04-15 05:54:37 +0100

    rest api setup

--- Backend/sales_logs/admin.py
+++ Backend/sales_logs/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import SalesLog
 
 # Register your models here.
+@admin.register(SalesLog)
+class SalesLogAdmin(admin.ModelAdmin):
+    list_display = ('uuid','agent', 'plan_sold', 'commision_made', 'init_date')
+    search_fields = ('agent', 'plan_sold', 'init_date')
--- Backend/sales_logs/models.py
+++ Backend/sales_logs/models.py
@@ -6,12 +6,12 @@ import uuid
 # Create your models here.
 
 class SalesLog(models.Model):
-    # date = models.CharField()  auto add now date
     uuid = models.UUIDField(
         db_index=True, default=uuid.uuid4, editable=False, primary_key=True)
     agent = models.OneToOneField(Agent)
     plan_sold = models.ForeignKey(Plan, on_delete=models.CASCADE)
     commission_made = models.PositiveIntegerField()
+    init_date = models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self):
         return self.uuid
